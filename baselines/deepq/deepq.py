b7bde9a58c1ef0e55732fb362cb0f83a64031f1a
Roberto Daluiso <roberto.daluiso@bancaimi.com>
2020-03-30 14:46:13 +0200

    Parametrize mean used to checkpoint deepq

--- baselines/deepq/deepq.py
+++ baselines/deepq/deepq.py
@@ -104,6 +104,7 @@ def learn(env,
           batch_size=32,
           print_freq=100,
           checkpoint_freq=10000,
+          checkpoint_episodes=100,
           checkpoint_path=None,
           learning_starts=1000,
           gamma=1.0,
@@ -151,6 +152,9 @@ def learn(env,
         how often to save the model. This is so that the best version is restored
         at the end of the training. If you do not wish to restore the best version at
         the end of the training set this variable to None.
+    checkpoint_episodes: int
+        how many episodes in the past are used to estimate the current mean reward.
+        Also used for logging.
     learning_starts: int
         how many steps of the model to collect transitions for before learning starts
     gamma: float
@@ -306,24 +310,24 @@ def learn(env,
                 # Update target network periodically.
                 update_target()
 
-            mean_100ep_reward = round(np.mean(episode_rewards[-101:-1]), 1)
+            mean_past_ep_reward = round(np.mean(episode_rewards[-(checkpoint_episodes+1):-1]), 1)
             num_episodes = len(episode_rewards)
             if done and print_freq is not None and len(episode_rewards) % print_freq == 0:
                 logger.record_tabular("steps", t)
                 logger.record_tabular("episodes", num_episodes)
-                logger.record_tabular("mean 100 episode reward", mean_100ep_reward)
+                logger.record_tabular("mean {} episode reward".format(checkpoint_episodes), mean_past_ep_reward)
                 logger.record_tabular("% time spent exploring", int(100 * exploration.value(t)))
                 logger.dump_tabular()
 
             if (checkpoint_freq is not None and t > learning_starts and
-                    num_episodes > 100 and t % checkpoint_freq == 0):
-                if saved_mean_reward is None or mean_100ep_reward > saved_mean_reward:
+                    num_episodes > checkpoint_episodes and t % checkpoint_freq == 0):
+                if saved_mean_reward is None or mean_past_ep_reward > saved_mean_reward:
                     if print_freq is not None:
                         logger.log("Saving model due to mean reward increase: {} -> {}".format(
-                                   saved_mean_reward, mean_100ep_reward))
+                                   saved_mean_reward, mean_past_ep_reward))
                     save_variables(model_file)
                     model_saved = True
-                    saved_mean_reward = mean_100ep_reward
+                    saved_mean_reward = mean_past_ep_reward
         if model_saved:
             if print_freq is not None:
                 logger.log("Restored model with mean reward: {}".format(saved_mean_reward))
